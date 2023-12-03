ba40bc920652b8d43c6e370e43bc1d92e15ecac8
Lonozx <lonozx@gmail.com>
2023-12-03 20:25:15 +0200

    added button to top

--- script/index.js
+++ script/index.js
@@ -1,5 +1,5 @@
-// Get the button:
-let mybutton = document.getElementById("myBtn");
+//Get the button:
+mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
@@ -16,4 +16,4 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-} 
+}
