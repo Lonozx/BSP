92afe6d2ca052739fac28da71654b1138ce6c5d9
Lonozx <lonozx@gmail.com>
2023-12-12 01:41:58 +0200

    some dropdown changes and form val now another view

--- script/validation.js
+++ script/validation.js
@@ -2,18 +2,20 @@ const submitButton = document.getElementById('submit');
 const name = document.querySelector('#name');
 const tel = document.querySelector('#tel');
 const check = document.querySelector('#check');
+const nameLabel = document.querySelector('#name-label');
+const phoneLabel = document.querySelector('#phone-label');
+
 
 function nameValidation() {
   if (!name.value) {
-    name.style.border = '1px solid';
-    name.style.borderColor = 'red';
+    nameLabel.style.color = 'red';
   }
 }
 
 function telValidation() {
   if (!tel.value) {
-    tel.style.border = '1px solid';
-    tel.style.borderColor = 'red';
+    phoneLabel.style.color = 'red';
+    
   }
 }
 function checkValidation() {
