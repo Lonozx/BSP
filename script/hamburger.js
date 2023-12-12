f73fbbcff6d693fad6963f24c6e783d163c7eeea
Lonozx <lonozx@gmail.com>
2023-12-13 00:09:10 +0200

    slightly updated mobile main page

--- script/hamburger.js
+++ script/hamburger.js
@@ -1,5 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("burger").addEventListener("click", function(){
         document.querySelector(".header").classList.toggle("open");
-    })
+        
+    }
+    
+    )
 })
+let phone = document.querySelector('.phone--ham');
+let burh = document.getElementById("burger");
+let header = document.querySelector(".header");
+function checkPhone(){
+    if(header.classList.contains('open')){
+        phone.style.color = 'white';
+    } else console.log('no.');
+}
+checkPhone();
