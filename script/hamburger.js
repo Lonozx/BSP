15c7abc8c53544729d0013e10f858a97fc40418a
Lonozx <lonozx@gmail.com>
2023-12-13 02:32:30 +0200

    minor changes

--- script/hamburger.js
+++ script/hamburger.js
@@ -1,17 +1,29 @@
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("burger").addEventListener("click", function(){
         document.querySelector(".header").classList.toggle("open");
+        let phone = document.getElementsByClassName("phone-ham");
+        let head = document.getElementById("hood");
+        if(head.classList.contains("open")){
+            phone.classList.add('wht');
+        } else console.log("no.");
         
     }
     
     )
 })
-let phone = document.querySelector('.phone--ham');
-let burh = document.getElementById("burger");
-let header = document.querySelector(".header");
-function checkPhone(){
-    if(header.classList.contains('open')){
-        phone.style.color = 'white';
-    } else console.log('no.');
-}
-checkPhone();
+
+// let phone = document.getElementsByClassName("phone--ham");
+// let burh = document.getElementById("burger");
+// let header = document.getElementsByClassName("header");
+// let head = document.getElementById("hood");
+// document.addEventListener("DOMContentLoaded", function(){
+//     function checkPhone(){
+//         if(head.classList.contains("open")){
+//             phone.classList.toggle("wht");
+//         } else console.log("no.");
+//         checkPhone();
+//     }
+// })
+
+
+
