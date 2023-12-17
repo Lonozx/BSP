022419241052ba3bbfb60f089a2c0e7b8a77afcc
Lonozx <lonozx@gmail.com>
2023-12-17 15:26:45 +0200

    update form

--- script/validation.js
+++ script/validation.js
@@ -20,7 +20,7 @@ function showPhoneLabel(){
     phoneLabel.style.visibility = 'hidden';
     tel.placeholder = 'Номер телефону';
   } 
-  // else if(regexTel.test(tel.value)){
+  // else if(!regexTel.match(tel.value)){
   //   phoneLabel.style.visibility = 'visible';
   //   phoneLabel.style.color = 'red';
   // }
@@ -58,8 +58,8 @@ function nameValidation(nm) {
 
 function telValidation() {
   // && regexTel.test(tel.value)
-  // let regexTel = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
-  if (!tel.value ) {
+  let regexTel = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+  if (!tel.value && !regexTel.test(tel.value)) {
     phoneLabel.style.visibility = 'visible';
     phoneLabel.style.color = 'red';
     tel.placeholder = '';
@@ -111,25 +111,25 @@ const checkMob = document.querySelector('#check-mob');
 const nameLabelMob = document.querySelector('#name-label-mob');
 const phoneLabelMob = document.querySelector('#phone-label-mob');
 
-function nameValidationMob() {
-  if (!nameMob.value) {
-    nameLabelMob.style.color = 'red';
-  }
-}
+// function nameValidationMob() {
+//   if (!nameMob.value) {
+//     nameLabelMob.style.color = 'red';
+//   }
+// }
 
-function telValidationMob() {
-  if (!telMob.value) {
-    phoneLabelMob.style.color = 'red';
+// function telValidationMob() {
+//   if (!telMob.value) {
+//     phoneLabelMob.style.color = 'red';
     
-  }
-}
-function checkValidationMob() {
-  if (!checkMob.checked) {
-    checkMob.style.border = '2px solid';
-    checkMob.style.borderColor = 'red';
+//   }
+// }
+// function checkValidationMob() {
+//   if (!checkMob.checked) {
+//     checkMob.style.border = '2px solid';
+//     checkMob.style.borderColor = 'red';
 
-  }
-}
+//   }
+// }
 
 submitMob.addEventListener('click', e => {
   e.preventDefault();
@@ -137,3 +137,95 @@ submitMob.addEventListener('click', e => {
   telValidationMob();
   checkValidationMob();
 });
+
+function showNameLabelMob(){
+  
+  if(nameMob.value==''){
+    nameLabelMob.style.visibility = 'hidden';
+    nameMob.placeholder = 'Ім\'я';
+  } else {nameLabelMob.style.visibility = 'visible';
+    nameMob.placeholder = '';
+}
+}
+function showPhoneLabelMob(){
+  let regexTel = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+  if(telMob.value==''){
+    phoneLabelMob.style.visibility = 'hidden';
+    telMob.placeholder = 'Номер телефону';
+  } 
+  else if(!regexTel.match(telMob.value)){
+    phoneLabelMob.style.visibility = 'visible';
+    phoneLabelMob.style.color = 'red';
+  }
+  
+  else {phoneLabelMob.style.visibility = 'visible';
+      telMob.placeholder = '';
+}
+}
+
+function nameValidationMob(nm) {
+  if (!nameMob.value) {
+    // nameLabel.classList.toggle('vis');
+    // nameLabel.style.visibility = 'visible';
+    // nameLabel.style.color = 'black';
+    nameLabelMob.style.visibility = 'visible';
+    nameLabelMob.style.color = 'red';
+    nameMob.placeholder = '';
+    nameLabelMob.style.transition = "all .2s ease-in-out";
+    setTimeout(function(){
+      nameLabelMob.style.visibility = 'visible';
+      nameLabelMob.style.color = 'black';
+      nameLabelMob.style.transition = "all .2s ease-in-out";
+      nameLabelMob.style.visibility = 'hidden';
+      nameMob.placeholder = 'Ім\'я';
+ }, 2000);
+  } else if(nameMob.value==''){
+    nameLabelMob.style.visibility = 'hidden';
+    nameLabelMob.style.color = 'transparent';
+    console.log('transparent')
+  }
+  else {
+    nameLabelMob.style.visibility = 'visible';
+  }
+}
+
+function telValidationMob() {
+  // && regexTel.test(tel.value)
+  let regexTel = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+  if (!telMob.value && !regexTel.test(telMob.value)) {
+    phoneLabelMob.style.visibility = 'visible';
+    phoneLabelMob.style.color = 'red';
+    telMob.placeholder = '';
+    phoneLabelMob.style.transition = "all .2s ease-in-out";
+    setTimeout(function(){
+      phoneLabelMob.style.visibility = 'visible';
+      phoneLabelMob.style.color = 'black';
+      phoneLabelMob.style.transition = "all .2s ease-in-out";
+      phoneLabelMob.style.visibility = 'hidden';
+      telMob.placeholder = 'Номер телефону';
+ }, 2000);
+  }
+  
+  else if(telMob.value==''){
+    phoneLabelMob.style.visibility = 'hidden';
+    phoneLabelMob.style.color = 'transparent';
+    console.log('transparent ph')
+  }
+  else {
+    phoneLabelMob.style.visibility = 'visible';
+  }
+}
+
+
+function checkValidationMob() {
+  if (!checkMob.checked) {
+    checkMob.style.border = '2px solid';
+    checkMob.style.borderColor = 'red';
+    checkMob.style.transition = "all .2s ease-in-out";
+    setTimeout(function(){
+      checkMob.style.border = '2px solid';
+      checkMob.style.borderColor = '#002F51';
+      checkMob.style.transition = "all .2s ease-in-out";
+ }, 2000);
+  }
+}
